7f6e712a36249f9379a99f743839d6568a765bfc
Nick Becker <nbeck90@uw.edu>
2015-03-12 15:56:40 -0700

    Add more cases for testing and raising errors in main function

--- hash.py
+++ hash.py
@@ -21,21 +21,25 @@ class HashTable(object):
         for i in self.table[hashed_key]:
             if i[0] == key:
                 return i[1]
+        else:
+            raise IndexError('Key not in bucket')
 
     def set(self, key, val):
         hashed_key = self.hash(key)
         self.table[hashed_key].append((key, val))
 
-if __name__ == '__main__':
-    import io
-    words = []
-    with io.open('/usr/share/dict/words', 'r') as word_file:
-        words = word_file.readlines()
+# if __name__ == '__main__':
+#     import io
+#     words = []
+#     with io.open('/usr/share/dict/words', 'r') as word_file:
+#         words = word_file.readlines()
 
-    t = HashTable()
-    for word in words:
-        t.set(word, word)
+#     t = HashTable()
+#     # for word in words:
+#     #     t.set(word, word)
 
-    print "bucket 600: {}".format(t.table[600])
-    print "word 654: {}".format(words[654])
-    print "word 65410: {}".format(words[65410])
+#     t.set('thing', 'bob')
+#     print t.get('hominy')
+#     # print "bucket 600: {}".format(t.table[600])
+#     # print "word 654: {}".format(words[654])
+#     # print "word 65410: {}".format(words[65410])
--- test_hash.py
+++ test_hash.py
@@ -1,5 +1,6 @@
 from hash import HashTable
 import io
+import pytest
 
 
 words = []
@@ -8,24 +9,44 @@ with io.open('/usr/share/dict/words', 'r') as word_file:
     words = word_file.readlines()
 
 
+def test_init():
+    ht = HashTable()
+    assert len(ht.table) == 1024
+    ht2 = HashTable(10000)
+    assert len(ht2.table) == 10000
+
+
 def test_hash():
-    t = HashTable()
-    t.set('coffee', 'coffee')
-    assert t.get('coffee') == 'coffee'
+    ht = HashTable()
+    ht.set('coffee', 'coffee')
+    assert ht.get('coffee') == 'coffee'
 
 
 def test_duplicate_hash_val():
-    t = HashTable()
-    t.set('bob', 'bob')
-    t.set('obb', 'obb')
-    assert t.get('bob') == 'bob'
-    assert t.get('obb') == 'obb'
+    ht = HashTable()
+    ht.set('bob', 'bob')
+    ht.set('obb', 'obb')
+    assert ht.get('bob') == 'bob'
+    assert ht.get('obb') == 'obb'
 
 
 def test_word_file():
-    t = HashTable()
+    ht = HashTable()
     for word in words:
-        t.set(word, word)
-    assert t.get(words[654]) == words[654]
-    assert t.get(words[3541]) == words[3541]
-    assert t.get(words[6541]) == words[6541]
+        ht.set(word, word)
+    assert ht.get(words[654]) == words[654]
+    assert ht.get(words[3541]) == words[3541]
+    assert ht.get(words[6541]) == words[6541]
+
+
+def test_non_item():
+    ht = HashTable()
+    ht.set('coffee', 'coffee')
+    with pytest.raises(IndexError):
+        ht.get('milk')
+
+
+def test_non_bucket():
+    ht = HashTable()
+    with pytest.raises(IndexError):
+        ht.table[1025]
