e2d80effa425bc12544f3bbf1ad5546b5af40572
James Warren <jwarren116@gmail.com>
2015-03-16 15:59:00 -0700

    Add timing to __name__ block

--- insert_sort.py
+++ insert_sort.py
@@ -8,3 +8,22 @@ def insert_sort(my_list):
         my_list[j + 1] = key
 
 if __name__ == '__main__':
+    import timeit
+
+    best_list = [i for i in range(5000)]
+    worst_list = [i for i in range(5000)][::-1]
+
+    def best_case():
+        return insert_sort(best_list)
+
+    def worst_case():
+        return insert_sort(worst_list)
+
+    print "Best case 5000 in order: {}".format(
+        timeit.timeit('best_case()', setup='from __main__ import best_case',
+                      number=100)
+    )
+    print "Best case 5000 reverse order: {}".format(
+        timeit.timeit('worst_case()', setup='from __main__ import worst_case',
+                      number=100)
+    )
