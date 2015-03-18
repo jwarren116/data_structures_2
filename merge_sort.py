2559a3b5ae765c70d1b8e85a1163d19ace04267f
James Warren <jwarren116@gmail.com>
2015-03-18 16:50:51 -0700

    Initial commit; adapted from book Fundamental Data Structures (tests not passing)

---
+++ merge_sort.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
+def merge_sort(lyst):
+    buf = [len(lyst)]
+    _merge_sort(lyst, buf, 0, len(lyst)-1)
+
+
+def _merge_sort(lyst, buf, low, high):
+    if low < high:
+        middle = (low + high) // 2
+        _merge_sort(lyst, buf, low, middle)
+        _merge_sort(lyst, buf, middle+1, high)
+        merge(lyst, buf, low, middle, high)
+
+
+def merge(lyst, buf, low, middle, high):
+    i1 = low
+    i2 = middle + 1
+
+    for i in range(low, high):
+        if i1 > middle:
+            buf[i] = lyst[i2]
+            i2 += 1
+        elif i2 > high:
+            buf[i] = lyst[i1]
+            i1 += 1
+        elif lyst[i1] < lyst[i2]:
+            buf[i] = lyst[i]
+            i1 += 1
+        else:
+            buf[i] = lyst[i2]
+            i2 += 1
+    for i in range(low, high):
+        lyst[i] = buf[i]
