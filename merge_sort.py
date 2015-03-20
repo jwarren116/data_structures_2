45f9d0085dc63f487737d011d328f94b128c3ae8
James Warren <jwarren116@gmail.com>
2015-03-19 20:45:00 -0700

    Add timing of merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -34,3 +34,37 @@ def merge(lyst, buf, low, middle, high):
             i2 += 1
     for i in range(low, high+1):
         lyst[i] = buf[i]
+
+
+if __name__ == '__main__':
+    import timeit
+    import random
+
+    best_list = [i for i in range(1000)]
+    worst_list = [i for i in range(1000)]
+    random.shuffle(worst_list)
+    tim_list = [i for i in range(1000)]
+    random.shuffle(tim_list)
+
+    def best_case():
+        return merge_sort(best_list)
+
+    def worst_case():
+        return merge_sort(worst_list)
+
+    print "Best case of {}: {}".format(len(best_list),
+                                       timeit.timeit('best_case()',
+                                       setup='from __main__ import best_case',
+                                       number=100)
+                                       )
+    print "Worst case of {}: {}".format(len(worst_list),
+                                        timeit.timeit('worst_case()',
+                                        setup='from __main__ import worst_case',
+                                        number=100)
+                                        )
+    print tim_list
+    print "Timsort! {}: {}\nWhoa...".format(len(tim_list),
+                                            timeit.timeit('tim_list.sort()',
+                                            setup='from __main__ import tim_list',
+                                            number=100)
+                                            )
