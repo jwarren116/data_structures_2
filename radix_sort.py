712ffa1078dc8da30de4edf9ced3d284bf2b34e6
James Warren <jwarren116@gmail.com>
2015-03-22 21:08:53 -0700

    Fix sorting with uppercase letters; add a rough timing block (worst case needs to be made worse)

--- radix_sort.py
+++ radix_sort.py
@@ -7,6 +7,8 @@ def int_radix(mylist):
     Sorts a list of ints by looking at the 1's place,
     the 10's, 100's, etc.
     '''
+    if len(mylist) <= 1:
+        return mylist
     for i in mylist:
         if (type(i) != int) or (i < 0):
             raise TypeError('Please input valid integers')
@@ -46,7 +48,7 @@ def str_radix(mylist):
             if place < len(string):
                 # put string in bucket based on ordinal value of
                 # iteration index, minus 96 (ordinal value 'a' - 1)
-                index = ord(string[place]) - 96
+                index = ord(string[place].lower()) - 96
             buckets[index].append(string)
         del mylist[:]
         for bucket in buckets:
@@ -56,8 +58,25 @@ def str_radix(mylist):
 
 
 if __name__ == '__main__':
-    l = ['tooth', 'dog', 'apple', 'rad', 'A', 'the', 'a', 'z', 'Peter']
-    print l
-    l = str_radix(l)
-    print l
+    import timeit
+
+    best_list = [i for i in range(5000)]
+    worst_list = [i for i in range(5000)][::-1]
+
+    def best_case():
+        return int_radix(best_list)
+
+    def worst_case():
+        return int_radix(worst_list)
+
+    print "Best case 5000 in order: {}".format(
+        timeit.timeit('best_case()',
+                      setup='from __main__ import best_case',
+                      number=100)
+    )
+    print "Worst case 5000 reverse order: {}".format(
+        timeit.timeit('worst_case()',
+                      setup='from __main__ import worst_case',
+                      number=100)
+    )
     
