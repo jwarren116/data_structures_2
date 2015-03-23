6b5f460f4af75adf99a5c0fb61b9845a3a822236
James Warren <jwarren116@gmail.com>
2015-03-22 20:34:04 -0700

    Outline basic radix sort for string; adapted from https://codehost.wordpress.com/2011/07/22/radix-sort/; not passing all preliminary tests with certain capitalization

--- radix_sort.py
+++ radix_sort.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+
 def int_radix(mylist):
     '''
     Sorts a list of ints by looking at the 1's place,
@@ -5,7 +9,7 @@ def int_radix(mylist):
     '''
     for i in mylist:
         if (type(i) != int) or (i < 0):
-            return 'Please input a valid list'
+            raise TypeError('Please input valid integers')
     if len(mylist) > 0:
         max_list = max(mylist)
         tens = 1
@@ -23,3 +27,37 @@ def int_radix(mylist):
             mylist = buff
             tens *= 10
         return mylist
+
+
+def str_radix(mylist):
+    '''Sorts a list of strings in lexicographic order'''
+    # return original list if it only contains one item
+    if len(mylist) <= 1:
+        return mylist
+    longest = 0
+    for string in mylist:
+        if len(string) > longest:
+            longest = len(string)
+    # interim buckets for sorting
+    buckets = [[] for i in range(27)]
+    for place in reversed(range(0, longest)):
+        for string in mylist:
+            index = 0 # properly sort shorter strings into 0 index
+            if place < len(string):
+                # put string in bucket based on ordinal value of
+                # iteration index, minus 96 (ordinal value 'a' - 1)
+                index = ord(string[place]) - 96
+            buckets[index].append(string)
+        del mylist[:]
+        for bucket in buckets:
+            mylist.extend(bucket)
+            del bucket[:]
+    return mylist
+
+
+if __name__ == '__main__':
+    l = ['tooth', 'dog', 'apple', 'rad', 'A', 'the', 'a', 'z', 'Peter']
+    print l
+    l = str_radix(l)
+    print l
+    
